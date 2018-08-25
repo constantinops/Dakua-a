655116cb93453fc6e41087432305af2e13d89026
constantino perez <constasophia@gmail.com>
2018-08-25 10:23:42 -0500

    se puso animacion a la imagen de foondo

--- app/src/main/java/com/example/aconst/dakuaa/SplashActivity.java
+++ app/src/main/java/com/example/aconst/dakuaa/SplashActivity.java
@@ -4,13 +4,24 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
 
 public class SplashActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        ImageView imageView=findViewById(R.id.icono);
+        Animation animation= AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fade);
+        imageView.startAnimation(animation);
+
+
+    /*
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -19,5 +30,26 @@ public class SplashActivity extends AppCompatActivity {
                 finish();
             }
         },900);
+        */
+
+        Thread time= new Thread(){
+            public void run(){
+
+                try {
+                    sleep(3000);
+                    Intent intent= new Intent(getApplicationContext(),MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                    super.run();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+
+        };
+        time.start();
+
     }
 }
